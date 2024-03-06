c30276bd82d11e79df77a363c3569a41ca74fca1
Tom Johnston <tom.johnston.2017@live.rhul.ac.uk>
2024-03-06 03:14:42 -0500

    adding more functions to test the debugger further

--- jscoursetesting/DebugCode/debug_code.js
+++ jscoursetesting/DebugCode/debug_code.js
@@ -3,8 +3,11 @@ function performOperation() {
     let num2 = parseInt(document.getElementById('input2').value);
 
     if (!isNaN(num1) && !isNaN(num2)) {
-        let result = multiply(num1, num2);
-        displayResult(result);
+        let results = [{function: multiply(num1, num2), label: 'Multiplication'},
+        {function: divide(num1, num2), label: 'Division'},
+        {function: add(num1, num2), label: 'Addition'},
+        {function: subtract(num1, num2), label: 'Subtraction'}];
+        displayResult(results);
     } else {
         displayResult('Please enter valid numbers');
     }
@@ -15,7 +18,23 @@ function multiply(a, b) {
     return a * b;
 }
 
-function displayResult(result) {
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+function divide(a, b) {
+    debugger;
+    return a / b;
+}
+
+function add(a, b) {
+    debugger;
+    return a + b;
+}
+
+function subtract(a, b) {
+    debugger;
+    return a - b;
+}
+
+function displayResult(results, typeOfResult) {
+    const resultElement = document.getElementById('results');
+    let resultsText = results.map((result, index) => `<p>The ${result.label} result is: ${result.function}</p>`);
+    resultElement.innerHTML = resultsText;
 }
