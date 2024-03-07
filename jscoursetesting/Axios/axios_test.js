85f9c4ceaa7cbb7f08c26224cd53748843575ddb
Tom Johnston <tom.johnston.2017@live.rhul.ac.uk>
2024-03-07 00:07:54 +0000

    testing asynchronous http requests using axios

---
+++ jscoursetesting/Axios/axios_test.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+
+/* axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    }); */
+
+async function fetchData() {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        console.log('Response:', response.data);
+    } catch(error) {
+        console.error('Error:', error);
+    }
+}
+
+fetchData();
